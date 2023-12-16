7e2fa4dd172480c3b9837378e46f965f746056cd
learnershailesh <123889793+learnershailesh@users.noreply.github.com>
2023-12-16 19:48:41 +0000

    variables and datatypes

---
+++ 01_basics/1_variable.js
@@ -0,0 +1,11 @@
+const accountId= 1234;
+let emailId = "shaileshadd@gmail.com";
+var emailpassword = "897623"
+accountcity= "jaipur"
+let accountState;
+
+emailId = "addict@gmail.com"
+emailpassword = "7594"
+accountcity = "Bangluru";
+
+console.table([accountId,emailId, emailpassword, accountcity, accountState])
---
+++ 01_basics/2_datatype.js
@@ -0,0 +1,7 @@
+let name = "Shailesh"
+let age = 20
+let loggedIn = false;
+
+console.log(typeof ("Shailesh"));
+console.log(typeof null); // null is a object type
+console.log(typeof undefined); // undefined is a undefined type
