7bd4ebf6218b53194ad04446b93baa3ea48a24f8
learnershailesh <123889793+learnershailesh@users.noreply.github.com>
2023-12-16 20:57:35 +0000

    convertions

---
+++ 01_basics/3_convertionOperation.js
@@ -0,0 +1,13 @@
+ let score = "23abd"
+
+ console.log(typeof score)
+
+ let inNumber = Number(score);
+
+ console.log(typeof inNumber);
+ console.log(inNumber)
+ // "23" => 23
+ // "23abs" => NaN
+ // null => 0
+ // true => 1
+ // false => 0
