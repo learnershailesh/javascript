d80ae2dba4f594e7b09b23914f9c4011ec4e1649
learnershailesh <123889793+learnershailesh@users.noreply.github.com>
2023-12-20 19:29:36 +0000

    json(javascript object notattion

--- 02_basics/10_objects.js
+++ 02_basics/10_objects.js
@@ -31,3 +31,34 @@ const regularUser = {
 }
 
 console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {
+    1: "a",
+    2: "b"
+}
+const obj2 ={
+    3: "c",
+    4: "d"
+}
+
+//const obj3 = console.log(Object.assign( {}, obj1, obj2));
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+console.log(user.hasOwnProperty('Islogin'));
+
+const {email: ww} = user;
+console.log(ww);
+/*{
+    "names": "skmaurya",
+    "course": "username",
+    "iuoefneoi": "fnei"
+}
+    api is look like this syntex
+*/
+
+
