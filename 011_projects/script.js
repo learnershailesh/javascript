d15e8b6c60aa7fce5a492023003b12ea21558db2
learnershailesh <123889793+learnershailesh@users.noreply.github.com>
2023-12-30 09:46:53 +0000

    project first

---
+++ 011_projects/script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('.button')
+
+const body = document.querySelector("body")
+
+buttons.forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id ==='grey'){
+            body.style.backgroundColor= e.target.id
+        }
+        if(e.target.id ==='yellow'){
+            body.style.backgroundColor= e.target.id
+        }
+        if(e.target.id ==='orange'){
+            body.style.backgroundColor= e.target.id
+        }
+        if(e.target.id ==='green'){
+            body.style.backgroundColor= e.target.id
+        }
+        if(e.target.id ==='blue'){
+            body.style.backgroundColor= e.target.id
+        }
+    })
+})
