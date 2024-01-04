72a46327cd3d5810bd92fe5bd9da7bbd4e6af93c
learnershailesh <123889793+learnershailesh@users.noreply.github.com>
2024-01-04 21:15:11 +0000

    Incomplete javascripts

---
+++ 014_projects/script.js
@@ -0,0 +1,7 @@
+const randomNumber = parseInt((Math.random())*100+1)
+const submit = document.querySelector("#Subt")
+const guessNumber = document.querySelector(".guessField")
+const result = document.querySelector(".lastResult")
+const lowhight = document.querySelector(".lowOrhigh")
+const gussesarea = document.querySelector(".guesses")
+
